188318fcc8f3f3d7143191ca849321be60925b9d
Oleg Latov <oleg@tgadigital.com>
2024-03-31 23:06:56 +0300

    Fluent UI

--- Chessy.Blazor/Program.cs
+++ Chessy.Blazor/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.FluentUI.AspNetCore.Components;
 using Chessy.Blazor.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddHttpClient();
+builder.Services.AddFluentUIComponents();
 
 var app = builder.Build();
 
