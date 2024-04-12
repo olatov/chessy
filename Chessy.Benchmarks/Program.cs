939e6e9b6e55009b156da0c356bf2343d1d792a8
Oleg Latov <oleg@tgadigital.com>
2024-04-13 01:09:28 +0300

    Basic benchmarks

---
+++ Chessy.Benchmarks/Program.cs
@@ -0,0 +1,50 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Chessy.Engine;
+using Chessy.Engine.Pieces;
+
+namespace Chessy.Benchmarks;
+
+public class PositionBenchmarks
+{
+    private Position _sut;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _sut = new Position();
+        _sut.ResetToStartingPosition();
+    }
+
+    [Benchmark]
+    public void GetMovesWithoutChecks()
+    {
+        _sut.GetMoves(PieceColor.White, skipChecks: true).ToArray();
+    }
+
+    [Benchmark]
+    public void GetMovesWithChecks()
+    {
+        _sut.GetMoves(PieceColor.White, skipChecks: false).ToArray();
+    }
+
+    [Benchmark]
+    public async Task FindBestMove()
+    {
+        var bestMove = await _sut.FindBestMoveABAsync(PieceColor.White, depth: 3);
+    }
+
+    [Benchmark]
+    public void BoardValue()
+    {
+        var _ = _sut.Board.MaterialValue;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        BenchmarkRunner.Run<PositionBenchmarks>();
+    }
+}
