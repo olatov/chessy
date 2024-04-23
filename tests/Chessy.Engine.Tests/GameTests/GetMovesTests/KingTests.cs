8064bd551ec86ae18a1fcd1b7a46e95483a92956
Oleg Latov <oleg@tgadigital.com>
2024-04-24 01:57:52 +0300

    More tests

--- tests/Chessy.Engine.Tests/GameTests/GetMovesTests/KingTests.cs
+++ tests/Chessy.Engine.Tests/GameTests/GetMovesTests/KingTests.cs
@@ -27,9 +27,246 @@ public sealed class KingTests
         var moves = _sut.GetMoves(king.Color);
 
         // Assert
-        var notations = moves.Select(m => m.GetNotationVariants().First());
+        var notations = moves.Select(m => m.GetNotationVariants()[0]);
         notations.Should().BeEquivalentTo(expectedMoves);
     }
 
-    // TODO: Add more tests
+
+    [Fact]
+    public void GetMoves_WhiteKingCannotMoveIntoCheck_ReturnsCorrectMoves()
+    {
+        // Arrange
+        _sut.Board["e4"] = Piece.CreateKing(PieceColor.White);
+        _sut.Board["d5"] = Piece.CreateQueen(PieceColor.Black);
+
+        // Act
+        var moves = _sut.GetMoves(PieceColor.White);
+
+        // Assert
+        var notations = moves.Select(m => m.GetNotationVariants()[0]);
+        notations.Should().NotContain("Ke5");
+    }
+
+    [Fact]
+    public void GetMoves_BlackKingCannotMoveIntoCheck_ReturnsCorrectMoves()
+    {
+        // Arrange
+        _sut.Board["e4"] = Piece.CreateKing(PieceColor.Black);
+        _sut.Board["d5"] = Piece.CreateQueen(PieceColor.White);
+
+        // Act
+        var moves = _sut.GetMoves(PieceColor.Black);
+
+        // Assert
+        var notations = moves.Select(m => m.GetNotationVariants()[0]);
+        notations.Should().NotContain("Ke5");
+    }
+
+
+    [Fact]
+    public void GetMoves_WhiteKingCanCastleShort_WhenCastlingRightsAreSet()
+    {
+        // Arrange
+        _sut.Board["e1"] = Piece.CreateKing(PieceColor.White);
+        _sut.Board["h1"] = Piece.CreateRook(PieceColor.White);
+        _sut.Board["a1"] = null;
+        _sut.Board["f1"] = null;
+        _sut.CastlingRights = new CastlingRights
+        {
+            WhiteLong = false,
+            WhiteShort = true
+        };
+
+        // Act
+        var moves = _sut.GetMoves(PieceColor.White);
+
+        // Assert
+        var notations = moves.Select(m => m.GetNotationVariants()[0]);
+        notations.Should().Contain("O-O");
+    }
+
+    [Fact]
+    public void GetMoves_WhiteKingCanCastleLong_WhenCastlingRightsAreSet()
+    {
+        // Arrange
+        _sut.Board["e1"] = Piece.CreateKing(PieceColor.White);
+        _sut.Board["a1"] = Piece.CreateRook(PieceColor.White);
+        _sut.Board["h1"] = null;
+        _sut.Board["f1"] = null;
+        _sut.CastlingRights = new CastlingRights
+        {
+            WhiteLong = true,
+            WhiteShort = false
+        };
+
+        // Act
+        var moves = _sut.GetMoves(PieceColor.White);
+
+        // Assert
+        var notations = moves.Select(m => m.GetNotationVariants()[0]);
+        notations.Should().Contain("O-O-O");
+    }
+
+    [Fact]
+    public void GetMoves_WhiteKingCannotCastle_WhenCastlingRightsAreNotSet()
+    {
+        // Arrange
+        _sut.Board["e1"] = Piece.CreateKing(PieceColor.White);
+        _sut.Board["a1"] = Piece.CreateRook(PieceColor.White);
+        _sut.Board["h1"] = Piece.CreateRook(PieceColor.White);
+        _sut.CastlingRights = new CastlingRights
+        {
+            WhiteLong = false,
+            WhiteShort = false
+        };
+
+        // Act
+        var moves = _sut.GetMoves(PieceColor.White);
+
+        // Assert
+        var notations = moves.Select(m => m.GetNotationVariants()[0]);
+        notations.Should().NotContain("O-O");
+        notations.Should().NotContain("O-O-O");
+    }
+
+
+    [Fact]
+    public void GetMoves_WhiteKingCannotCastle_WhenKingMoved()
+    {
+        // Arrange
+        var king = _sut.Board["e2"] = Piece.CreateKing(PieceColor.White);
+        _sut.Board["a1"] = Piece.CreateRook(PieceColor.White);
+        _sut.Board["h1"] = Piece.CreateRook(PieceColor.White);
+        _sut.CastlingRights = new CastlingRights
+        {
+            WhiteLong = true,
+            WhiteShort = false
+        };
+
+        _sut.MakeMove(Move.For(king, Coords.Parse("e2"), Coords.Parse("e1")));
+
+        // Act
+        var moves = _sut.GetMoves(PieceColor.White);
+
+        // Assert
+        var notations = moves.Select(m => m.GetNotationVariants()[0]);
+        notations.Should().NotContain("O-O-O");
+        notations.Should().NotContain("O-O");
+    }
+
+
+    [Fact]
+    public void GetMoves_WhiteKingCannotCastle_WhenInCheck()
+    {
+        // Arrange
+        _sut.Board["e1"] = Piece.CreateKing(PieceColor.White);
+        _sut.Board["a1"] = Piece.CreateRook(PieceColor.White);
+        _sut.Board["h1"] = Piece.CreateRook(PieceColor.White);
+        _sut.Board["e8"] = Piece.CreateQueen(PieceColor.Black);
+        _sut.CastlingRights = new CastlingRights
+        {
+            WhiteLong = true,
+            WhiteShort = true
+        };
+
+        // Act
+        var moves = _sut.GetMoves(PieceColor.White);
+
+        // Assert
+        var notations = moves.Select(m => m.GetNotationVariants()[0]);
+        notations.Should().NotContain("O-O");
+        notations.Should().NotContain("O-O-O");
+    }
+
+
+    [Fact]
+    public void GetMoves_WhiteKingCannotCastleShort_WhenFSquareAttacked()
+    {
+        // Arrange
+        _sut.Board["e1"] = Piece.CreateKing(PieceColor.White);
+        _sut.Board["h1"] = Piece.CreateRook(PieceColor.White);
+        _sut.Board["f1"] = null;
+        _sut.Board["f8"] = Piece.CreateRook(PieceColor.Black);
+        _sut.CastlingRights = new CastlingRights
+        {
+            WhiteLong = false,
+            WhiteShort = true
+        };
+
+        // Act
+        var moves = _sut.GetMoves(PieceColor.White);
+
+        // Assert
+        var notations = moves.Select(m => m.GetNotationVariants()[0]);
+        notations.Should().NotContain("O-O");
+    }
+
+
+    [Fact]
+    public void GetMoves_WhiteKingCannotCastleLong_WhenDSquareAttacked()
+    {
+        // Arrange
+        _sut.Board["e1"] = Piece.CreateKing(PieceColor.White);
+        _sut.Board["a1"] = Piece.CreateRook(PieceColor.White);
+        _sut.Board["d1"] = null;
+        _sut.Board["d8"] = Piece.CreateRook(PieceColor.Black);
+        _sut.CastlingRights = new CastlingRights
+        {
+            WhiteLong = true,
+            WhiteShort = false
+        };
+
+        // Act
+        var moves = _sut.GetMoves(PieceColor.White);
+
+        // Assert
+        var notations = moves.Select(m => m.GetNotationVariants()[0]);
+        notations.Should().NotContain("O-O-O");
+    }
+
+
+    [Fact]
+    public void GetMoves_WhiteKingCannotCastleLong_IntoCheck()
+    {
+        // Arrange
+        _sut.Board["e1"] = Piece.CreateKing(PieceColor.White);
+        _sut.Board["a1"] = Piece.CreateRook(PieceColor.White);
+        _sut.Board["h1"] = Piece.CreateRook(PieceColor.White);
+        _sut.Board["c8"] = Piece.CreateQueen(PieceColor.Black);
+        _sut.CastlingRights = new CastlingRights
+        {
+            WhiteLong = true,
+            WhiteShort = false
+        };
+
+        // Act
+        var moves = _sut.GetMoves(PieceColor.White);
+
+        // Assert
+        var notations = moves.Select(m => m.GetNotationVariants()[0]);
+        notations.Should().NotContain("O-O-O");
+    }
+
+
+    [Fact]
+    public void GetMoves_WhiteKingCannotCastleShort_IntoCheck()
+    {
+        // Arrange
+        _sut.Board["e1"] = Piece.CreateKing(PieceColor.White);
+        _sut.Board["h1"] = Piece.CreateRook(PieceColor.White);
+        _sut.Board["f1"] = null;
+        _sut.Board["f8"] = Piece.CreateQueen(PieceColor.Black);
+        _sut.CastlingRights = new CastlingRights
+        {
+            WhiteLong = false,
+            WhiteShort = true
+        };
+
+        // Act
+        var moves = _sut.GetMoves(PieceColor.White);
+
+        // Assert
+        var notations = moves.Select(m => m.GetNotationVariants()[0]);
+        notations.Should().NotContain("O-O");
+    }
 }
--- tests/Chessy.Engine.Tests/GameTests/GetMovesTests/QueenTests.cs
+++ tests/Chessy.Engine.Tests/GameTests/GetMovesTests/QueenTests.cs
@@ -2,7 +2,7 @@ namespace Chessy.Engine.Tests.GameTests.GetMovesTests;
 
 public sealed class QueenTests
 {
-    private Game _sut = new();
+    private readonly Game _sut = new();
 
     public QueenTests()
     {
@@ -22,5 +22,174 @@ public sealed class QueenTests
         moves.Should().HaveCount(27);
     }
 
-    // TODO: Add more tests
+
+    [Fact]
+    public void ShouldCaptureDiagonally_WhenWhite()
+    {
+        // Arrange
+        var queen = _sut.Board["e5"] = Piece.CreateQueen(PieceColor.White);
+        _sut.Board["d6"] = Piece.CreatePawn(PieceColor.Black);
+        _sut.Board["f6"] = Piece.CreatePawn(PieceColor.Black);
+
+        // Act
+        var moves = _sut.GetMoves(queen.Color);
+
+        // Assert
+        var notations = moves.Select(m => m.GetNotationVariants()[0]);
+        notations.Should().Contain("Qxd6");
+        notations.Should().Contain("Qxf6");
+    }
+
+    [Fact]
+    public void ShouldCaptureDiagonally_WhenBlack()
+    {
+        // Arrange
+        var queen = _sut.Board["e4"] = Piece.CreateQueen(PieceColor.Black);
+        _sut.Board["d5"] = Piece.CreatePawn(PieceColor.White);
+        _sut.Board["f5"] = Piece.CreatePawn(PieceColor.White);
+
+        // Act
+        var moves = _sut.GetMoves(queen.Color);
+
+        // Assert
+        var notations = moves.Select(m => m.GetNotationVariants()[0]);
+        notations.Should().Contain("Qxd5");
+        notations.Should().Contain("Qxf5");
+    }
+
+    [Fact]
+    public void ShouldCaptureAlongRows_WhenWhite()
+    {
+        // Arrange
+        var queen = _sut.Board["e4"] = Piece.CreateQueen(PieceColor.White);
+        _sut.Board["e5"] = Piece.CreatePawn(PieceColor.Black);
+        _sut.Board["e3"] = Piece.CreatePawn(PieceColor.Black);
+
+        // Act
+        var moves = _sut.GetMoves(queen.Color);
+
+        // Assert
+        var notations = moves.Select(m => m.GetNotationVariants()[0]);
+        notations.Should().Contain("Qxe5");
+        notations.Should().Contain("Qxe3");
+    }
+
+    [Fact]
+    public void ShouldCaptureAlongRows_WhenBlack()
+    {
+        // Arrange
+        var queen = _sut.Board["e4"] = Piece.CreateQueen(PieceColor.Black);
+        _sut.Board["e5"] = Piece.CreatePawn(PieceColor.White);
+        _sut.Board["e3"] = Piece.CreatePawn(PieceColor.White);
+
+        // Act
+        var moves = _sut.GetMoves(queen.Color);
+
+        // Assert
+        var notations = moves.Select(m => m.GetNotationVariants()[0]);
+        notations.Should().Contain("Qxe5");
+        notations.Should().Contain("Qxe3");
+    }
+
+    [Fact]
+    public void ShouldCaptureAlongFiles_WhenWhite()
+    {
+        // Arrange
+        var queen = _sut.Board["d4"] = Piece.CreateQueen(PieceColor.White);
+        _sut.Board["e4"] = Piece.CreatePawn(PieceColor.Black);
+        _sut.Board["c4"] = Piece.CreatePawn(PieceColor.Black);
+
+        // Act
+        var moves = _sut.GetMoves(queen.Color);
+
+        // Assert
+        var notations = moves.Select(m => m.GetNotationVariants()[0]);
+        notations.Should().Contain("Qxe4");
+        notations.Should().Contain("Qxc4");
+    }
+
+    [Fact]
+    public void ShouldCaptureAlongFiles_WhenBlack()
+    {
+        // Arrange
+        var queen = _sut.Board["d4"] = Piece.CreateQueen(PieceColor.Black);
+        _sut.Board["e4"] = Piece.CreatePawn(PieceColor.White);
+        _sut.Board["c4"] = Piece.CreatePawn(PieceColor.White);
+
+        // Act
+        var moves = _sut.GetMoves(queen.Color);
+
+        // Assert
+        var notations = moves.Select(m => m.GetNotationVariants()[0]);
+        notations.Should().Contain("Qxe4");
+        notations.Should().Contain("Qxc4");
+    }
+
+    [Fact]
+    public void ShouldGiveCheck_WhenWhite()
+    {
+        // Arrange
+        var queen = _sut.Board["a1"] = Piece.CreateQueen(PieceColor.White);
+        _sut.Board["e3"] = Piece.CreateKing(PieceColor.Black);
+
+        // Act
+        var moves = _sut.GetMoves(queen.Color);
+
+        // Assert
+        var notations = moves.Select(m => m.GetNotationVariants()[0]);
+        notations.Should().Contain("Qe1+");
+        moves.Should().Contain(m => m.IsCheck);
+    }
+
+    [Fact]
+    public void ShouldGiveCheck_WhenBlack()
+    {
+        // Arrange
+        var queen = _sut.Board["a8"] = Piece.CreateQueen(PieceColor.Black);
+        _sut.Board["e6"] = Piece.CreateKing(PieceColor.White);
+
+        // Act
+        var moves = _sut.GetMoves(queen.Color);
+
+        // Assert
+        var notations = moves.Select(m => m.GetNotationVariants()[0]);
+        notations.Should().Contain("Qe8+");
+        moves.Should().Contain(m => m.IsCheck);
+    }
+
+    [Fact]
+    public void ShouldGiveCheckmate_WhenWhite()
+    {
+        // Arrange
+        var queen = _sut.Board["a1"] = Piece.CreateQueen(PieceColor.White);
+        _sut.Board["h8"] = Piece.CreateKing(PieceColor.Black);
+        _sut.Board["g7"] = Piece.CreatePawn(PieceColor.Black);
+        _sut.Board["h7"] = Piece.CreatePawn(PieceColor.Black);
+
+        // Act
+        var moves = _sut.GetMoves(queen.Color);
+
+        // Assert
+        var notations = moves.Select(m => m.GetNotationVariants()[0]);
+        notations.Should().Contain("Qa8#");
+        moves.Should().Contain(m => m.IsCheckmate);
+    }
+
+    [Fact]
+    public void ShouldGiveCheckmate_WhenBlack()
+    {
+        // Arrange
+        var queen = _sut.Board["a8"] = Piece.CreateQueen(PieceColor.Black);
+        _sut.Board["h1"] = Piece.CreateKing(PieceColor.White);
+        _sut.Board["g2"] = Piece.CreatePawn(PieceColor.White);
+        _sut.Board["h2"] = Piece.CreatePawn(PieceColor.White);
+
+        // Act
+        var moves = _sut.GetMoves(queen.Color);
+
+        // Assert
+        var notations = moves.Select(m => m.GetNotationVariants()[0]);
+        notations.Should().Contain("Qa1#");
+        moves.Should().Contain(m => m.IsCheckmate);
+    }
 }
